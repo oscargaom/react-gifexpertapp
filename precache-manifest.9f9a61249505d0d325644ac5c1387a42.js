b10bb87bb06fc390559befb5f8b68c1910b23a0e
Oscar Manuel <oscargaom@gmail.com>
2020-06-28 14:09:37 -0500

    First commit

---
+++ precache-manifest.9f9a61249505d0d325644ac5c1387a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebff9c6c7d870d8d7fde3fcd543119e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6724dc2318e79ea8d6d0",
+    "url": "/static/css/main.56d3db6b.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6724dc2318e79ea8d6d0",
+    "url": "/static/js/main.32d1aa82.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
