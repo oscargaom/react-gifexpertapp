59ace98d0dddb3d05d2b2517e273652414c45a76
Oscar Manuel <oscargaom@gmail.com>
2020-07-12 15:32:43 -0500

    Se agregaron comentarios de documentacion a las pruebas

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,10 +5,12 @@ import { AddCategory } from '../../components/AddCategory';
 
 describe('Pruebas unitarias del componente <AddCategory/>', () => {
 
+    // En el momento de la inicialización setCategories es undefined.
     const setCategories = jest.fn();
     let wrapper = shallow(<AddCategory setCategories={setCategories}/>);
 
     beforeEach( () => {
+        // Clears the mock.calls and mock.instances properties of all mocks
         jest.clearAllMocks();
         wrapper = shallow(<AddCategory setCategories={setCategories}/>);
     });
@@ -34,7 +36,11 @@ describe('Pruebas unitarias del componente <AddCategory/>', () => {
         form.simulate("submit", {
             preventDefault: () => {}
         });
-
+        /*  Como el valor de inputValue es vacío la función setCategories no es 
+            llamada, y como inicialmente el valor de setCategories es igual a jest.fn()
+            y este es igual a undefined, se valida que no haya cambiado su valor y 
+            se determine que la función no fue llamada.
+        */
         expect(setCategories).not.toHaveBeenCalled();
     })
     
@@ -42,6 +48,7 @@ describe('Pruebas unitarias del componente <AddCategory/>', () => {
         const input = wrapper.find("input");
         const value = "Submit test";
         
+        // Se va a cambiar el valor del inputValue
         input.simulate("change", { target: { value } });
         
         const form = wrapper.find("form");
@@ -50,6 +57,9 @@ describe('Pruebas unitarias del componente <AddCategory/>', () => {
             preventDefault: () => {}
         });
         
+        /*  Se espera que setCategories se haya llamda porque el valor del inputValue
+            no es vacío.
+        */
         expect(setCategories).toHaveBeenCalled();
     })
 
@@ -69,8 +79,12 @@ describe('Pruebas unitarias del componente <AddCategory/>', () => {
             preventDefault: () => {}
         });
 
-        
+        // Se espera que solamente se haya realizado el llamado una sola vez        
         expect(setCategories).toHaveBeenCalledTimes(1);
+
+        /*  Use .toHaveBeenCalledWith to ensure that a mock function was called with specific arguments.
+            Por lo que se espera que setCategories haya realizado un llamado a una función (setCategories(cat => [inputValue, ...cat]);)
+        */
         expect(setCategories).toBeCalledWith( expect.any(Function) );
         expect(wrapper.find('input').prop('value')).toBe("");
     })
